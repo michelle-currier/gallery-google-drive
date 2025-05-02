4b5f7358412016c07b6c9538fd67eb6387a51247
michelle-currier <mushel@gmail.com>
2025-05-02 10:36:01 -0500

    found the other component that was linked on the gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,10 @@
-import FolderImages from "./../components/FolderImages";
+import GalleryAPI from "./../components/Gallery4";
 
 export default function Gallery() {
   return (
     <div className="bg-gray-800 text-white flex flex-col gap-8 p-6">
       <h1>Gallery tah dahhhh</h1>
-      <FolderImages />
+      <GalleryAPI />
     </div>
   );
 }
